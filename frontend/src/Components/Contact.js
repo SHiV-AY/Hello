0f0102f67f6a97f89d91314a33f8004a47efeed0
SHiV-AY <shubhamwadhave7@gmail.com>
2023-06-21 14:43:13 +0530

    Contact Cards

--- frontend/src/Components/Contact.js
+++ frontend/src/Components/Contact.js
@@ -1,11 +1,8 @@
 import * as React from 'react';
-import Button from '@mui/material/Button';
-import CssBaseline from '@mui/material/CssBaseline';
-import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
+import PhoneInTalkOutlinedIcon from '@mui/icons-material/PhoneInTalkOutlined';
+import ForwardToInboxOutlinedIcon from '@mui/icons-material/ForwardToInboxOutlined';
+import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
 import Link from '@mui/material/Link';
-import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
@@ -42,100 +39,153 @@ function Contact() {
   };
 
   return (
-    <div style={{display:"flex",justifyContent: "left"}}>
-          <div style={{width:"30%",margin:"2%"}}>
+    <div>
 
-      <ThemeProvider theme={defaultTheme}>
-        <Container maxWidth={false}  component="main" >
-          <Box
+      <div style={{ display: "flex", justifyContent: "left" }}>
+        <div style={{ width: "20%", marginLeft: "10%" }}>
 
-            sx={{
+          <ThemeProvider theme={defaultTheme}>
+            <Container maxWidth={false} component="main" style={{ width: "100%" }} >
+
+              <Box
+
+                sx={{
 
 
-              marginTop: 3,
-              display: 'flex',
-              flexDirection: 'column',
-              alignItems: 'center',
-              boxShadow: 6,
-              opacity: 10,
-              maxWidth: 'false',
-              backgroundImage: 'url("https://wallpapers.com/images/hd/popular-film-your-name-anime-vrf8kphnyzh4o0bk.jpg")',
-              backgroundPositionX:'-90px',
-             backgroundPositionY:'-400px'
-            }}
+                  marginTop: 3,
+                  display: 'flex',
+                  flexDirection: 'column',
+                  alignItems: 'center',
+                  boxShadow: 6,
+                  maxWidth: 'false',
+                  backgroundImage: 'url("https://wallpapers.com/images/hd/popular-film-your-name-anime-vrf8kphnyzh4o0bk.jpg")',
+                  backgroundPositionX: '-90px',
+                  backgroundPositionY: '-400px'
+                }}
 
-          >
+              >
+                <div style={{ display: "flex", justifyContent: "left", marginLeft: "40px" }}>
+                  <ForwardToInboxOutlinedIcon fontSize='small' style={{ marginTop: "5px", marginRight: '5px' }} />
 
-            <h2>Phone</h2>
-          </Box>
+                  <div style={{ width: "30%", marginRight: "50px" }}>
+                    <Typography variant="h6" style={{ fontSize: "12px" }}>
+                      <span style={{marginLeft:"15px"}}>Phone</span><br />+9158574810
 
-        </Container>
-      </ThemeProvider>
+                    </Typography>
+                  </div>
+                </div>
+              </Box>
+
+            </Container>
+          </ThemeProvider>
+
+        </div>
+        <div style={{ width: "20%", marginLeft: "10%" }}>
+
+          <ThemeProvider theme={defaultTheme}>
+            <Container maxWidth={false} component="main" style={{ width: "100%" }} >
+
+              <Box
+
+                sx={{
+
+
+                  marginTop: 3,
+                  display: 'flex',
+                  flexDirection: 'column',
+                  alignItems: 'center',
+                  boxShadow: 6,
+                  maxWidth: 'false',
+                  backgroundImage: 'url("https://wallpapers.com/images/hd/popular-film-your-name-anime-vrf8kphnyzh4o0bk.jpg")',
+                  backgroundPositionX: '-90px',
+                  backgroundPositionY: '-400px'
+                }}
+
+              >
+                <div style={{ display: "flex", justifyContent: "left", marginLeft: "40px" }}>
+                  <ForwardToInboxOutlinedIcon fontSize='small' style={{ marginTop: "5px", marginRight: '5px' }} />
+
+                  <div style={{ width: "30%", marginRight: "50px" }}>
+                    <Typography variant="h6" style={{ fontSize: "12px" }}>
+                    <span style={{marginLeft:"60px"}}>Email</span><br />Shubhamwadhave7@gmail.com
+
+                    </Typography>
+                  </div>
+                </div>
+              </Box>
+
+            </Container>
+          </ThemeProvider>
+
+        </div>
+        <div style={{ width: "20%", marginLeft: "10%" }}>
+
+          <ThemeProvider theme={defaultTheme}>
+            <Container maxWidth={false} component="main" style={{ width: "100%" }} >
+
+              <Box
+
+                sx={{
+
+
+                  marginTop: 3,
+                  display: 'flex',
+                  flexDirection: 'column',
+                  alignItems: 'center',
+                  boxShadow: 6,
+                  maxWidth: 'false',
+                  backgroundImage: 'url("https://wallpapers.com/images/hd/popular-film-your-name-anime-vrf8kphnyzh4o0bk.jpg")',
+                  backgroundPositionX: '-90px',
+                  backgroundPositionY: '-400px'
+                }}
+
+              >
+                <div style={{ display: "flex", justifyContent: "left", marginLeft: "40px" }}>
+                  <HomeOutlinedIcon fontSize='small' style={{ marginTop: "5px", marginRight: '10px' }} />
+
+                  <div style={{ width: "60%", marginRight: "50px" }}>
+                    <Typography variant="h6" style={{ fontSize: "12px" }}>
+                    <span style={{marginLeft:"15px"}}>Address</span><br />Andheri Mumbai
+
+                    </Typography>
+                  </div>
+                </div>
+              </Box>
+
+            </Container>
+          </ThemeProvider>
+
+        </div>
+
 
       </div>
-      <div style={{width:"30%",margin:"2%"}}>
+      <div style={{ border: 'solid' }}>
+        <ThemeProvider theme={defaultTheme}>
+          <Container maxWidth={false} component="main" >
+            <Box
+
+              sx={{
 
 
+                marginTop: 3,
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+                boxShadow: 6,
+                opacity: 10,
+                maxWidth: 'false',
+                backgroundImage: 'url("https://wallpapers.com/images/hd/popular-film-your-name-anime-vrf8kphnyzh4o0bk.jpg")',
+                backgroundPositionX: '-90px',
+                backgroundPositionY: '-400px'
+              }}
 
-      <ThemeProvider theme={defaultTheme}>
-        <Container maxWidth={false} component="main" >
-          <Box
-
-            sx={{
-
-
-              marginTop: 3,
-              display: 'flex',
-              flexDirection: 'column',
-              alignItems: 'center',
-              boxShadow: 6,
-              opacity: 10,
-              maxWidth: 'false',
-              backgroundImage: 'url("https://wallpapers.com/images/hd/popular-film-your-name-anime-vrf8kphnyzh4o0bk.jpg")',
-             backgroundPositionX:'-90px',
-             backgroundPositionY:'-400px'
-
-            }}
-
-          >
-
-            <h2>Email</h2>
-          </Box>
-
-        </Container>
-      </ThemeProvider>
-
-      </div>
-      <div style={{width:"30%",margin:"2%"}}>
+            >
 
 
-      <ThemeProvider theme={defaultTheme}>
-        <Container maxWidth={false} component="main" >
-          <Box
+            </Box>
 
-            sx={{
-
-
-              marginTop: 3,
-              display: 'flex',
-              flexDirection: 'column',
-              alignItems: 'center',
-              boxShadow: 6,
-              opacity: 10,
-              maxWidth: 'false',
-              backgroundImage: 'url("https://wallpapers.com/images/hd/popular-film-your-name-anime-vrf8kphnyzh4o0bk.jpg")',
-              backgroundPositionX:'-90px',
-             backgroundPositionY:'-400px'
-            }}
-
-          >
-
-            <h2>Address</h2>
-          </Box>
-
-        </Container>
-      </ThemeProvider>
-
+          </Container>
+        </ThemeProvider>
       </div>
     </div>
 
